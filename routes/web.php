499fdfc4a249bffacb6d0b4e420efc9f69844977
Hadi Pd <h7ad3i@chmail.ir>
2021-10-22 08:53:01 +0330

    Start git in project

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SiteController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('site.boot');
+// });
+Route::get('/',[SiteController::class,'index']);
+Route::get('logout',[LoginController::class,'logout']);
+
+
+Route::middleware(['auth', 'isAdmin'])->group(function() {
+Route::get('admin',[AdminController::class,'index'])->name('admin');
+Route::resource('category', CategoryController::class);
+
+
+ 
+});
